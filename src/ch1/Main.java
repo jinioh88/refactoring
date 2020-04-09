eec9d405998c7e43a7732f03899af1e5ae934047
jinio <jinioh88@gmail.com>
2020-04-10 00:11:47 +0900

    리팩토링

---
+++ src/ch1/Main.java
@@ -0,0 +1,11 @@
+package ch1;
+
+public class Main {
+    public static void main(String[] args) {
+        Robot robot = new Robot("Andrew");
+        robot.order(Robot.COMMAND_WALK);
+        robot.order(Robot.COMMAND_STOP);
+        robot.order(Robot.COMMAND_JUMP);
+        robot.order(11);
+    }
+}
--- src/ch1/Robot.java
+++ src/ch1/Robot.java
@@ -1,6 +1,10 @@
 package ch1;
 
 public class Robot {
+    public static final int COMMAND_WALK = 0;
+    public static final int COMMAND_STOP = 1;
+    public static final int COMMAND_JUMP = 2;
+
     private final String  _name;
 
     public Robot(String name) {
@@ -8,11 +12,11 @@ public class Robot {
     }
 
     public void order(int command) {
-        if(command == 0) {
+        if(command == COMMAND_WALK) {
             System.out.println(_name + " works.");
-        } else if(command == 1) {
+        } else if(command == COMMAND_STOP) {
             System.out.println(_name + " stops.");
-        } else if(command == 2) {
+        } else if(command == COMMAND_JUMP) {
             System.out.println(_name + " jumps.");
         } else {
             System.out.println("Command error. command = " + command);
