7ada3a44a81ae1b5de141407f0e3246587ba30ff
jinio <jinioh88@gmail.com>
2020-04-10 21:37:25 +0900

    제어플래그 그랙토링 전

--- src/ch2/Main.java
+++ src/ch2/Main.java
@@ -1,13 +1,23 @@
 package ch2;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+
 public class Main {
     public static void main(String[] args) {
-        int[] data = {1, 9, 0, 2, 8, 5, 6, 3, 4, 7};
-
-        if(FindInt.find(data, 5)) {
-            System.out.println("Found!");
-        } else {
-            System.out.println("Not found...");
+        try {
+            SimpleDatabase db = new SimpleDatabase(new FileReader("dbfile.txt"));
+            Iterator<String> it = db.iterator();
+            while(it.hasNext()) {
+                String key = it.next();
+                System.out.println("KEY: \"" + key + "\"");
+                System.out.println("VALUE: \"" + db.getValue(key) + "\"");
+                System.out.println();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
