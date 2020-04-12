3ceadeeeec94a7d76bbabcf2f3af8609313be168
jinio <jinioh88@gmail.com>
2020-04-12 15:44:21 +0900

    추가 설명

--- src/ch9/Logger.java
+++ src/ch9/Logger.java
@@ -1,11 +1,48 @@
 package ch9;
 
 public class Logger {
+    private enum State {
+        STOPPED {
+            @Override
+            public void start() {
+                System.out.println("** START LOGGING **");
+            }
+
+            @Override
+            public void stop() {}
+
+            @Override
+            public void log(String info) {
+                System.out.println("Ignoring: " + info);
+            }
+        },
+
+        LOGGING {
+            @Override
+            public void start() {
+            }
+
+            @Override
+            public void stop() {
+                System.out.println("** STOP LOGGING **");
+            }
+
+            @Override
+            public void log(String info) {
+                System.out.println("Logging: " + info);
+            }
+        };
+
+        public abstract void start();
+        public abstract void stop();
+        public abstract void log(String info);
+    }
+
 
     private State state;
 
     public Logger() {
-        setState(State.STATE_STOPPED);
+        setState(State.STOPPED);
     }
 
     public State getState() {
@@ -18,12 +55,12 @@ public class Logger {
 
     public void start() {
         state.start();
-        setState(State.STATE_LOGGING);
+        setState(State.LOGGING);
     }
 
     public void stop() {
         state.stop();
-        setState(State.STATE_STOPPED);
+        setState(State.STOPPED);
     }
 
     public void log(String info) {
