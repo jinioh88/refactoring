45a07e7eea131bfecdf9c4b90c6821720cf8aa95
jinio <jinioh88@gmail.com>
2020-04-12 14:51:16 +0900

    리팩토링 전

---
+++ src/ch9/Logger.java
@@ -0,0 +1,54 @@
+package ch9;
+
+public class Logger {
+    public static final int STATE_STOPPED = 0;
+    public static final int STATE_LOGGING = 1;
+
+    private int state;
+
+    public Logger() {
+    }
+
+    public Logger(int state) {
+        this.state = state;
+    }
+
+    public void start() {
+        switch (state) {
+            case STATE_STOPPED:
+                System.out.println("** START LOGGING **");
+                state = STATE_LOGGING;
+                break;
+            case STATE_LOGGING:
+                break;
+            default:
+                System.out.println("Invalid state: " + state);
+        }
+    }
+
+    public void stop() {
+        switch (state) {
+            case STATE_STOPPED:
+                break;
+            case STATE_LOGGING:
+                System.out.println("** STOP LOGGING **");
+                state = STATE_STOPPED;
+                break;
+            default:
+                System.out.println("Invalid state: " + state);
+        }
+    }
+
+    public void log(String info) {
+        switch (state) {
+            case STATE_STOPPED:
+                System.out.println("Ignoring: " + info);
+                break;
+            case STATE_LOGGING:
+                System.out.println("Logging: " + info);
+                break;
+            default:
+                System.out.println("Invalid state: " + state);
+        }
+    }
+}
